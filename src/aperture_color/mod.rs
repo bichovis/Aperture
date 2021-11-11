97bd83438325cd160ade33eef0f79d2636be30bc
bichovis <elcorreodevisi@yahoo.es>
2021-11-11 15:36:26 +0100

    Created a sepparated mod for the interface colors (in progress)

---
+++ src/aperture_color/mod.rs
@@ -0,0 +1,6 @@
+use sdl2::pixels::Color;
+
+pub fn get_background() -> Color {
+    let background_color = Color::RGB(128, 128, 128);
+    background_color
+}
--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,59 @@
 extern crate sdl2;
 
-use sdl2::pixels::Color;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 use std::time::Duration;
+mod aperture_color;
 
 pub fn main() {
     let mut fullscreen = false;
-    let background_color = Color::RGB(128, 128, 128);
     let sdl_context = sdl2::init().unwrap();
     let video_subsystem = sdl_context.video().unwrap();
 
-    let window = video_subsystem.window("rust-sdl2 demo", 800, 600)
+    let window = video_subsystem
+        .window("rust-sdl2 demo", 800, 600)
         .position_centered()
         .build()
         .unwrap();
 
     let mut canvas = window.into_canvas().build().unwrap();
 
-    canvas.set_draw_color(background_color);
+    canvas.set_draw_color(aperture_color::get_background());
     canvas.clear();
     canvas.present();
     let mut event_pump = sdl_context.event_pump().unwrap();
     'running: loop {
-        canvas.set_draw_color(background_color);
+        canvas.set_draw_color(aperture_color::get_background());
         canvas.clear();
         for event in event_pump.poll_iter() {
             match event {
-                Event::Quit {..} |
-                Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
-                    break 'running
-                },
-                Event::KeyDown { keycode: Some(Keycode::F), .. } => {
+                Event::Quit { .. }
+                | Event::KeyDown {
+                    keycode: Some(Keycode::Escape),
+                    ..
+                } => break 'running,
+                Event::KeyDown {
+                    keycode: Some(Keycode::F),
+                    ..
+                } => {
                     if !fullscreen {
-                        match canvas.window_mut().set_fullscreen(sdl2::video::FullscreenType::Desktop) {
+                        match canvas
+                            .window_mut()
+                            .set_fullscreen(sdl2::video::FullscreenType::Desktop)
+                        {
                             Ok(_) => fullscreen = true,
-                            Err(_) => ()
+                            Err(_) => (),
                         }
                     } else {
-                        match canvas.window_mut().set_fullscreen(sdl2::video::FullscreenType::Off) {
+                        match canvas
+                            .window_mut()
+                            .set_fullscreen(sdl2::video::FullscreenType::Off)
+                        {
                             Ok(_) => fullscreen = false,
-                            Err(_) => ()
+                            Err(_) => (),
                         }
                     }
-                },
+                }
                 _ => {}
             }
         }
